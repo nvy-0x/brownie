d9c313b733cdf51861038d016bb1d7a57178197f
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-06-01 15:06:28 -0400

    Update flattener.py

--- brownie/project/flattener.py
+++ brownie/project/flattener.py
@@ -86,7 +86,7 @@ class Flattener:
         """The flattened source code for use verifying."""
         flattened_deps = toposort_flatten(self.dependencies)
         # all source files in the correct order for concatenation
-        sources = list(map(self.sources.__getitem__, flattened_deps))
+        sources = [self.sources[x] for x in flattened_deps]
         # all pragma statements, we already have the license used + know which compiler
         # version is used via the build info
         pragmas = set(map(str.strip, mapcat(PRAGMA_PATTERN.findall, sources)))
@@ -95,12 +95,12 @@ class Flattener:
             "", LICENSE_PATTERN.sub("", IMPORT_PATTERN.sub("", src))
         )
 
-        sources = (f"// File: {file}\n\n{wipe(src)}" for src, file in zip(sources, flattened_deps))
+        source_strings = (f"// File: {file}\n\n{wipe(src)}" for src, file in zip(sources, flattened_deps))
 
         flat = (
             "\n".join(pragma for pragma in pragmas if "pragma solidity" not in pragma)
             + "\n\n"
-            + "\n".join(sources)
+            + "\n".join(source_strings)
         )
         # hopefully this doesn't mess up anything pretty, but just gotta remove all
         # that extraneous whitespace
@@ -147,11 +147,11 @@ class Flattener:
         if path.is_absolute():
             return path.as_posix()
 
-        source_file_dir = Path(source_file_dir).resolve()
-        newpath = (source_file_dir / path).resolve()
+        source_file_path = Path(source_file_dir).resolve()
+        newpath = (source_file_path / path).resolve()
         while not newpath.exists():
-            source_file_dir = source_file_dir.parent
-            newpath = (source_file_dir / path).resolve()
-            if source_file_dir == Path("/"):
+            source_file_path = source_file_path.parent
+            newpath = (source_file_path / path).resolve()
+            if source_file_path == Path("/"):
                 raise FileNotFoundError(f"Cannot determine location of {import_path}")
         return newpath.as_posix()
